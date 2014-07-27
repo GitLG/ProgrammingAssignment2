e637bc8adfc7b7cdaf384d4a73073bf5184340f7
LG <lalit2@gmail.com>
2014-07-27 14:54:48 -0700

    matrix solver with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##The first function, makeVector creates a special "matrix store", which is really a list containing a function to
+##set the value of the matrix
+##get the value of the matrix
+##set the value of the matrix inverse
+##get the value of the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 	  s <- NULL
@@ -20,6 +21,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## function to calculate inverse of special matrix with function above
+## retrive stored inverse if exists; else calculate it
 
 cacheSolve <- function(x, ...) {
 	## Return a matrix that is the inverse of 'x'
